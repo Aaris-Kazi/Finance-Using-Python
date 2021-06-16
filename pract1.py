044f0ab72a8f95e963a6f3aeee32587176ced837
Aaris-Kazi <aariskazi@gmail.com>
2021-06-16 05:07:17 -0700

    now can plot in matplotlib

--- pract1.py
+++ pract1.py
@@ -6,4 +6,14 @@ symbol = 'GOOGL'
 ticker = yf.Ticker(symbol)
 # print(ticker)
 ticker_df = ticker.history(period= '1M', interval= '1D',start= '2021-05-15', end='2021-06-15')
-print(ticker_df)
+# print(ticker_df['Volume'].shape)
+days = pd.date_range(start= '2021-05-17', end='2021-06-05')
+# print(days.shape)
+# volume = ticker_df['Volume']
+stocks = pd.DataFrame({
+    "Date": days,
+    "Volume": ticker_df['Volume']
+})
+# print(stocks)
+plt.plot(stocks['Date'], stocks['Volume'])
+plt.show()
