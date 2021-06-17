0cfd7d9c499cd872eeaf47e70d5750e7548ec1e7
Aaris-Kazi <aariskazi@gmail.com>
2021-06-17 05:36:03 -0700

    right data output

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from finance_value import finance
 import pandas as pd
 import numpy as np
-from plotter import actual
+from plotter import *
 from sklearn.linear_model import LinearRegression
 
-# finance()
-df = pd.read_csv('google.csv')
-# print(df.info())
-actual(df['Date'], df['Volume'])
+def main():
+    # finance()
+    df = pd.read_csv('google.csv')
+    # print(df.info())
+    # actual(df['Date'], df['Close'])
+    overall(df)
+    
+if __name__ == "__main__":
+    main()
+
--- plotter.py
+++ plotter.py
@@ -3,3 +3,8 @@ def actual(x, y):
     plt.grid(True)
     plt.plot(x, y)
     plt.show()
+
+def overall(df):
+    plt.grid(True)
+    df.plot()
+    plt.show()
