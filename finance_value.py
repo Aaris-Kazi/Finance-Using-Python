d5a7ed989043eb35c4017390a073cb4d95a2df33
Aaris-Kazi <aariskazi@gmail.com>
2021-06-17 03:40:01 -0700

    testing and implementing

--- finance_value.py
+++ finance_value.py
@@ -1,7 +1,10 @@
 import yfinance as yf
+import datetime
+
+# print(datetime.date.today())
 def finance():
     symbol = 'GOOGL'
     ticker = yf.Ticker(symbol)
     # print(ticker)
-    ticker_df = ticker.history(period= '1M', interval= '1D',start= '2021-05-15', end='2021-06-15')
+    ticker_df = ticker.history(period= '1M', interval= '1D',start= '2021-05-15', end=datetime.date.today())
     return ticker_df
