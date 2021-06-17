d87ce0ded53b3f59c2bdf241298e644da97793e0
Aaris-Kazi <aariskazi@gmail.com>
2021-06-17 04:34:07 -0700

    to export in csv

--- finance_value.py
+++ finance_value.py
@@ -1,10 +1,15 @@
 import yfinance as yf
 import datetime
-
+import pandas_datareader.data as web
 # print(datetime.date.today())
 def finance():
     symbol = 'GOOGL'
     ticker = yf.Ticker(symbol)
     # print(ticker)
     ticker_df = ticker.history(period= '1M', interval= '1D',start= '2021-05-15', end=datetime.date.today())
+    ticker_df.to_csv('google.csv')
     return ticker_df
+
+def bypd():
+    df = web.DataReader('TSLA', 'yahoo', '2021-05-15', datetime.date.today())
+    df.to_csv('TSLA.csv')
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 import datetime
-
+import pandas as pd
+import pandas_datareader.data as web
 print(datetime.date.today())
+df = web.DataReader('TSLA', 'yahoo', '2021-05-15', datetime.date.today())
+df.to_csv('TSLA.csv')
