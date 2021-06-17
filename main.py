be5cc2231147a8366476f64abeb816b8cbe828f2
Aaris-Kazi <aariskazi@gmail.com>
2021-06-17 06:54:33 -0700

    optimization

--- main.py
+++ main.py
@@ -5,19 +5,18 @@ from plotter import *
 from sklearn.linear_model import LinearRegression
 
 def main():
-    banner = "1: To load csv (GOOGLE)\n 2: Get Info \n 3: Graph of Stonks \n 3: Graph of Overall"
+    banner = "1: To load csv (GOOGLE)\n 2: Get Info \n 3: Graph of Stonks \n 4: Graph of Overall"
     print(banner)
     s = int(input('Select Value:'))
+    df = pd.read_csv('google.csv')
     if s == 1:
         finance()
     elif s == 2:
-        df = pd.read_csv('google.csv')
         print(df.info())
     elif s == 3:
-        df = pd.read_csv('google.csv')
         actual(df['Date'], df['Close'])
     elif s == 4:
-        df = pd.read_csv('google.csv')
+        
         overall(df)
     
 if __name__ == "__main__":
